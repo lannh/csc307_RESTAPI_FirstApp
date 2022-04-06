71346be139d0c527e35dee8634593d6516e9d6f4
Lan Nguyen <lannguyen@Lans-MacBook-Pro.local>
2022-04-05 19:17:55 -0700

    adding find users by name

--- backend.js
+++ backend.js
@@ -41,8 +41,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if(name != undefined)
+    {
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else
+    {
+        res.send(users);
+    }
 });
+
+const findUserByName = (name) =>
+{
+    return users['users_list'].filter( (user) => user['name'] === name);
+}
 
 app.listen(port, () => {
     console.log('Example listening at http://localhost:${port}');
