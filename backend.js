588ef64fd54c08daf1a413c93a8a8511a2ea2955
Lan Nguyen <lannguyen@Lans-MacBook-Pro.local>
2022-04-05 18:35:25 -0700

    initial commit

---
+++ backend.js
@@ -0,0 +1,14 @@
+const { application } = require('express');
+const express = require('express');
+const app = express();
+const port = 5001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}');
+});
